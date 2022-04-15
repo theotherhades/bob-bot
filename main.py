cbb0f17e5f47c7bffd23857faa1841ebdb8b3deb
theotherhades <64885838+theotherhades@users.noreply.github.com>
2022-04-15 18:29:57 +1200

    Added economy stuff, needs finishing

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import os
+import json
+import random
+import nextcord
 from nextcord.ext import commands
 
 client = commands.Bot(command_prefix = ['*', 'bob '])
+
+# CONSTANTS
+EMBED_COLORS = [nextcord.Color.red(), nextcord.Color.yellow(), nextcord.Color.blurple()]
 
 # heroku why u no work?
 
@@ -15,9 +21,11 @@ async def on_ready():
 async def on_command_error(ctx, error):
     # No perms?
     if isinstance(error, commands.MissingPermissions):
-        await ctx.reply(f'Nice try, {ctx.author.mention}\nYou\'ll need perms for that to work.')
+        await ctx.reply(f':smiling_imp: Nice try, {ctx.author.mention}\nYou\'ll need perms for that to work.')
     else:
         await ctx.reply(f':thinking: Hmm... something went wrong.\nThis is the error I got: `{error}`')
+
+
 
 # COMMANDS
 @client.command('test', help = 'Just for testing, nothing special')
@@ -28,5 +36,36 @@ async def test(ctx):
 @commands.has_permissions(administrator = True)
 async def permtest(ctx):
     await ctx.reply('it worked :triumph:')
+
+
+# ECONOMY STUFF
+@client.command('balance', help = 'Check user balance')
+async def balance(ctx):
+    await new_user(ctx.author)
+    economy_dict = await getdata()
+    
+    bal_em = nextcord.Embed(title = f'Balance for {ctx.author.name}', color = random.choice(EMBED_COLORS))
+    bal_em.add_field(name = 'Purse', value = economy_dict[str(user.id)]['purse'])
+    bal_em.add_field(name = 'Bank', value = economy_dict[str(user.id)]['bank'])
+
+    await ctx.reply(embed = bal_em)
+
+async def new_user(user):
+    economy_dict = await getdata()
+
+    if str(user.id) in economy_dict:
+        return False
+    else:
+        economy_dict[str(user.id)]['purse'] = 0
+        economy_dict[str(user.id)]['bank'] = 0
+
+    with open('much_dinero.json', 'w') as f:
+        economy_dict = json.dump(economy_dict, f)
+
+async def getdata():
+    with open('much_dinero.json', 'r') as f:
+        return json.load(f)
+
+
 
 client.run(os.environ['CLIENT_TOKEN'])
