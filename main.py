cf17204b554bcf7311b05eba42643a2aff8b4595
theotherhades <64885838+theotherhades@users.noreply.github.com>
2022-04-15 17:27:43 +1200

    updating bot output a bit

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ client = commands.Bot(command_prefix = ['*', 'bob '])
 # EVENTS
 @client.event
 async def on_ready():
-    print(f'yeah whatever I\'m online as {client.user.name}')
+    print(f'I\'m online as {client.user.name}')
 
 # COMMANDS
 @client.command('test')
 async def test(ctx):
-    await ctx.send('hi, hope you\'re doing ok.')
+    await ctx.send('bob test')
 
 client.run(os.environ['CLIENT_TOKEN'])
