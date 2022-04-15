c27c18420aa6274cc47c6835331f6d31ba5c22a1
theotherhades <64885838+theotherhades@users.noreply.github.com>
2022-04-15 17:54:10 +1200

    some nextcord tests

--- main.py
+++ main.py
@@ -10,9 +10,23 @@ client = commands.Bot(command_prefix = ['*', 'bob '])
 async def on_ready():
     print(f'I\'m online as {client.user.name}')
 
+# Handling errors
+@client.event
+async def on_command_error(ctx, error):
+    # No perms?
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.reply(f'Nice try, {ctx.author.mention}\nYou\'ll need perms for that to work.')
+    else:
+        await ctx.reply(f':thinking: Hmm... something went wrong.\nThis is the error I got: `{error}`')
+
 # COMMANDS
-@client.command('test')
+@client.command('test', help = 'Just for testing, nothing special')
 async def test(ctx):
-    await ctx.send('bob test')
+    await ctx.reply('if you\'re seeing this, something probably went right')
+
+@client.command('permtest', help = 'testing perms')
+@commands.has_permissions(administrator = True)
+async def permtest(ctx):
+    await ctx.reply('it worked :triumph:')
 
 client.run(os.environ['CLIENT_TOKEN'])
